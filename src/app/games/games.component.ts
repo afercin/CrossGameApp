4aea28cc1caa8c633b9032b8794e5b416662ce8a
afercin <afercin@gmail.com>
2022-05-01 21:23:14 +0200

    removed electron service and added controller service

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,19 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { IpcService } from '../services/ipc.service';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RestService } from '../services/rest.service';
 import { Game } from '../../types/game';
+import { GamepadService } from 'ngx-gamepad';
+
+enum GamepadButtons {
+    A = "button0",
+    B = "button1",
+    Y = "button2",
+    X = "button3",
+
+    L = "button4",
+    R = "button5",
+    LT = "button6",
+    RT = "button7",
+
+    Select = "button8",
+    Start = "button9",
+    PS = "button10",
+
+    LY = "button11",
+    RY = "button12",
+
+    Up = "up3",
+    Down = "down3",
+    Left = "left3",
+    Right = "right3",
+
+    L_Up = "up0",
+    L_Down = "down0",
+    L_Left = "left0",
+    L_Right = "right0",
+
+    R_Up = "left2",
+    R_Down = "right2",
+    R_Left = "up1",
+    R_Right = "down1",
+
+    LT_Pressed = "right1",
+    LT_Released = "left1",
+
+    RT_Pressed = "down2",
+    RT_Released = "up2",
+}
 
 @Component({
     selector: 'app-games',
     templateUrl: './games.component.html',
-    styleUrls: ['./games.component.css']
+    styleUrls: ['./games.component.css'],
+    providers: [
+        RestService,
+        GamepadService
+    ]
 })
 export class GamesComponent implements OnInit {
 
-    games?: Game[];
-    selectedGame?: Game | any;
+    games: Game[] = [];
+    selectedGame: number = 0;
 
-    constructor(private restService: RestService, private ipcService: IpcService) { }
+    constructor(private restService: RestService, private gamepad: GamepadService, private cdRef: ChangeDetectorRef) { }
 
     ngOnInit(): void {
         this.restService.getGames().subscribe({
@@ -34,10 +78,54 @@ export class GamesComponent implements OnInit {
             },
             error: (err) => console.log(`Request failed with error: ${err}`)
         });
+        
+        this.gamepad.connect().subscribe(() => {
+            
+            this.gamepad.after(GamepadButtons.Select).subscribe(() => this.options());
+            this.gamepad.after(GamepadButtons.Start).subscribe(() => this.reload());
+
+            this.gamepad.after(GamepadButtons.A).subscribe(() => this.play());
+            this.gamepad.after(GamepadButtons.B).subscribe(() => this.back());
+
+            this.gamepad.after(GamepadButtons.Left).subscribe(() => this.moveLeft());
+            this.gamepad.after(GamepadButtons.L_Left).subscribe(() => this.moveLeft());
+
+            this.gamepad.after(GamepadButtons.Right).subscribe(() => this.moveRight());
+            this.gamepad.after(GamepadButtons.L_Right).subscribe(() => this.moveRight());
+        })
+    }
+
+    options(): void {
+        console.log("Openning options");
+    }
+    
+    play(): void {
+        this.launchGame(this.games[this.selectedGame])
+    }
+
+    reload(): void {
+        console.log("Reload page");
+    }
+    
+    back(): void {
+        console.log("Returning to main menu");
+    }
+
+    moveRight(): void {
+        this.selectedGame += 1;
+        this.selectedGame %= this.games.length;
+        this.cdRef.detectChanges();
+    }
+
+    moveLeft(): void {
+        this.selectedGame -= 1;
+        if (this.selectedGame < 0)
+            this.selectedGame = this.games.length -1;
+        this.cdRef.detectChanges();
     }
 
     launchGame(game: Game): void {
-        this.ipcService.send("launch_game", game)
+        console.log(`Playing ${game.name}`);        
     }
 
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { IpcService } from '../services/ipc.service';
 import { RestService } from '../services/rest.service';
 
 @Component({
@@ -14,33 +13,10 @@ export class MainComponent implements OnInit {
   password: string = "";
   object: object | any;
 
-  constructor(private ipcService: IpcService, private restService: RestService, private cdRef: ChangeDetectorRef) { }
+  constructor(private restService: RestService, private cdRef: ChangeDetectorRef) { }
 
 
   ngOnInit(): void {
-    this.ipcService.on("log", (event: any, arg: string) => {
-        this.printMsg(arg);
-    });
-
-    this.ipcService.on("info", (event: any, arg: string) => {
-        this.printMsg(`<span class="info">${arg}</span>`);
-    });
-
-    this.ipcService.on("warn", (event: any, arg: string) => {
-        this.printMsg(`<span class="warn">${arg}</span>`);
-    });
-
-    this.ipcService.on("error", (event: any, arg: string) => {
-        this.printMsg(`<span class="error">${arg}</span>`);
-    });
-
-    this.ipcService.on("get_credentials", (event: any, arg: any) => {
-        this.username = arg[0] == "" ? this.username : arg[0];
-        this.password = arg[1] == "" ? this.password : arg[1];
-        this.cdRef.detectChanges();
-    });
-
-    //this.ipcService.send("get_credentials");
 }
 
 printMsg(msg: string): void {
@@ -51,11 +27,6 @@ printMsg(msg: string): void {
 }
 
 ngOnDestroy(): void {
-    this.ipcService.removeAllListeners("log");
-    this.ipcService.removeAllListeners("info");
-    this.ipcService.removeAllListeners("error");
-    this.ipcService.removeAllListeners("warn");
-    this.ipcService.removeAllListeners("credentials");
 }
 
 incur(): void {
@@ -79,8 +50,7 @@ incur(): void {
 }
 
 report(): void {
-    this.message = "";
-    this.ipcService.send("button_click", ["report", this.username, this.password]);
+    
 }
 
 }
