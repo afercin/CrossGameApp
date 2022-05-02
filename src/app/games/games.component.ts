24c0cb61ffcf5387a220a9d6c20bf06e9b86bab6
afercin <afercin@gmail.com>
2022-05-02 12:38:10 +0200

    changed controller to keyboard events

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,63 +1,24 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RestService } from '../services/rest.service';
 import { Game } from '../../types/game';
-import { GamepadService } from 'ngx-gamepad';
-
-enum GamepadButtons {
-    A = "button0",
-    B = "button1",
-    Y = "button2",
-    X = "button3",
-
-    L = "button4",
-    R = "button5",
-    LT = "button6",
-    RT = "button7",
-
-    Select = "button8",
-    Start = "button9",
-    PS = "button10",
-
-    LY = "button11",
-    RY = "button12",
-
-    Up = "up3",
-    Down = "down3",
-    Left = "left3",
-    Right = "right3",
-
-    L_Up = "up0",
-    L_Down = "down0",
-    L_Left = "left0",
-    L_Right = "right0",
-
-    R_Up = "left2",
-    R_Down = "right2",
-    R_Left = "up1",
-    R_Right = "down1",
-
-    LT_Pressed = "right1",
-    LT_Released = "left1",
-
-    RT_Pressed = "down2",
-    RT_Released = "up2",
-}
 
 @Component({
     selector: 'app-games',
     templateUrl: './games.component.html',
     styleUrls: ['./games.component.css'],
     providers: [
-        RestService,
-        GamepadService
-    ]
+        RestService
+    ],
+    host: {
+        '(document:keypress)': 'handleKeyboardEvent($event)'
+    }
 })
 export class GamesComponent implements OnInit {
 
     games: Game[] = [];
     selectedGame: number = 0;
 
-    constructor(private restService: RestService, private gamepad: GamepadService, private cdRef: ChangeDetectorRef) { }
+    constructor(private restService: RestService, private cdRef: ChangeDetectorRef) { }
 
     ngOnInit(): void {
         this.restService.getGames().subscribe({
@@ -78,21 +39,17 @@ export class GamesComponent implements OnInit {
             },
             error: (err) => console.log(`Request failed with error: ${err}`)
         });
-        
-        this.gamepad.connect().subscribe(() => {
-            
-            this.gamepad.after(GamepadButtons.Select).subscribe(() => this.options());
-            this.gamepad.after(GamepadButtons.Start).subscribe(() => this.reload());
+    }
 
-            this.gamepad.after(GamepadButtons.A).subscribe(() => this.play());
-            this.gamepad.after(GamepadButtons.B).subscribe(() => this.back());
-
-            this.gamepad.after(GamepadButtons.Left).subscribe(() => this.moveLeft());
-            this.gamepad.after(GamepadButtons.L_Left).subscribe(() => this.moveLeft());
-
-            this.gamepad.after(GamepadButtons.Right).subscribe(() => this.moveRight());
-            this.gamepad.after(GamepadButtons.L_Right).subscribe(() => this.moveRight());
-        })
+    handleKeyboardEvent(event: KeyboardEvent) {
+        switch (event.key){
+            case "Enter": this.play(); break;
+            case "q": this.back(); break;
+            case "a": this.moveLeft(); break;
+            case "d": this.moveRight(); break;
+            case "o": this.options(); break;
+            case "r": this.reload(); break;
+        }
     }
 
     options(): void {
